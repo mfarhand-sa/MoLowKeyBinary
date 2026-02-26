b5348b1369812dd1d8522e4c09099515776eebbb
MoLowKey <hello@mohi.dev>
2026-02-26 15:02:11 -0800

    Update MoLowKey target to use URL and checksum

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MoLowKey",
-            path: "Artifacts/MoLowKey.xcframework"
+            url: "https://github.com/mfarhand-sa/MoLowKeyBinary/releases/download/1.0.0/MoLowKey.xcframework.zip",
+            checksum: "73664754ffbfb84a50cbc1b3c8af95f1c6bcd0a5cb41355054decd0e2a65bee2"
         )
     ]
 )
